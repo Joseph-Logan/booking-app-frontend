637cee3e76ca557a096fc4a251ed53836ee6f0fb
logan wolverine <jarm061198@gmail.com>
2020-11-08 12:11:07 -0600

    update constants

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,4 +1,6 @@
-const URL_ENDPOINT = 'http://localhost:4000/api'
+// const URL_ENDPOINT = 'http://localhost:4000/api'
+
+const URL_ENDPOINT = 'https://app-booking-back.herokuapp.com/api'
 
 export {
   URL_ENDPOINT
