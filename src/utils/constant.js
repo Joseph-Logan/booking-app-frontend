e17cf8977af6b20785f3279dce0812655debc4a6
Kitcha Rosales <kitcharosales11@gmail.com>
2020-11-09 18:29:33 +0100

    cambios

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,4 +1,4 @@
-const URL_ENDPOINT = 'http://localhost:4000/api'
+const URL_ENDPOINT = 'http://app-booking-back.herokuapp.com/api/'
 
 // const URL_ENDPOINT = 'https://app-booking-back.herokuapp.com/api'
 
